95f09358c5f5bb356e3ec22e04bc7563cd45371c
7620AtharvaKulkarni <kulkarniatharva753@gmail.com>
2022-07-08 11:37:14 +0530

    login with css

---
+++ WEEK 1/DAY 4/new.js
@@ -0,0 +1,17 @@
+// function fun(){
+//     alert("external js")
+// } 
+let name
+function get(){
+    username= document.getElementById("username").value; 
+    password= document.getElementById("password").value;
+
+    if((username==="Atharva")&& password=="12345"){
+        alert("login successful")
+    
+    }
+    else
+        alert("login failed")
+           
+
+}
