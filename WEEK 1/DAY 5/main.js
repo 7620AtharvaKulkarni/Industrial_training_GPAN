6a0a400a3e1d0798686907611a71b0d91d0f2157
7620AtharvaKulkarni <kulkarniatharva753@gmail.com>
2022-07-09 17:19:07 +0530

    using bookstrap

---
+++ WEEK 1/DAY 5/main.js
@@ -0,0 +1,16 @@
+// function login(){
+//     alert("external js")
+// } 
+let name
+function get(){
+    username= document.getElementById("username").value; 
+    password= document.getElementById("password").value;
+
+    if((username=="Atharva")&& (password=="12345")){
+        alert("login successful")
+    
+    }
+    else
+        alert("login failed")
+           
+    }
