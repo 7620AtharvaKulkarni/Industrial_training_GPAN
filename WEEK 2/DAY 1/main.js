ac65962a3043863edb4ad0082fdcf8b5d471590f
7620AtharvaKulkarni <kulkarniatharva753@gmail.com>
2022-07-11 16:53:35 +0530

    create account page

---
+++ WEEK 2/DAY 1/main.js
@@ -0,0 +1,12 @@
+let name
+function get(){
+    first-ame= document.getElementById("First name").value; 
+    Last-name= document.getElementById("password").value;
+    email-id= document.getElementById("email id").value;
+    phone-no.= document.getElementById("mob").value;
+    clg-name= document.getElementById("name").value;
+    
+
+    document.write(first-name + "<br/>" +last-name +"<br/>" +email-id + "<br/>" +phone-no. "<br/>" +clgname);
+           
+    }
